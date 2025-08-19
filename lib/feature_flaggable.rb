0f63ec0fd99e7b3bf2dbb2e59072019bcabc4045
Carl Smith <carl@llwebconsulting.com>
2025-08-18 23:05:54 -0400

    set up core config and railtie

--- lib/feature_flaggable.rb
+++ lib/feature_flaggable.rb
@@ -1,8 +1,19 @@
 # frozen_string_literal: true
 
 require_relative "feature_flaggable/version"
+require_relative "feature_flaggable/config"
+require_relative "feature_flaggable/railtie" if defined?(Rails::Railtie)
 
 module FeatureFlaggable
   class Error < StandardError; end
-  # Your code goes here...
+
+  class << self
+    def config
+      @config ||= Config.new
+    end
+
+    def configure
+      yield config if block_given?
+    end
+  end
 end
---
+++ lib/feature_flaggable/config.rb
@@ -0,0 +1,24 @@
+# frozen_string_literal: true
+
+require 'logger'
+
+module FeatureFlaggable
+  class Config
+    attr_accessor :config_path, :env, :reload_on_change, :cache_backend, :logger, :scope_resolver
+
+    def initialize
+      @config_path = "config/feature_flags.yml"
+      @env = nil
+      @reload_on_change = false
+      @cache_backend = :none
+      @logger = defined?(Rails) && Rails.respond_to?(:logger) ? Rails.logger : Logger.new($stdout)
+      @scope_resolver = ->(user) do
+        if user.respond_to?(:user_type)
+          user.user_type&.to_sym
+        elsif user.respond_to?(:role)
+          user.role&.to_sym
+        end
+      end
+    end
+  end
+end
